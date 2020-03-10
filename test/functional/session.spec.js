582115a1968018b7c406542ad1734d98ee08e713
Daniel Costa Carvalho <daniel.carvalho@ftc.edu.br>
2020-03-10 11:59:51 -0300

    refatorando: retirando variáveis desnecessárias

--- test/functional/session.spec.js
+++ test/functional/session.spec.js
@@ -1,6 +1,6 @@
 'use strict'
 
-const { test, trait} = use('Test/Suite')('Session')
+const { test, trait } = use('Test/Suite')('Session')
 
 trait('Test/ApiClient')
 
@@ -13,7 +13,7 @@ test('Retorna um token quando se autentica', async ({ assert, client }) => {
     password: '123'
   }
 
-  const user = await User.create(baseUser)
+  await User.create(baseUser)
 
   const response = await client.post('/sessions')
     .send(baseUser)
