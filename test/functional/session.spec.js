512cd25cc93fda46a076db97a13e05af1f9c01b1
Daniel Costa Carvalho <danielcostacarvalho@gmail.com>
2020-03-10 20:55:59 -0300

    Garantindo que ao inserir um email inválido, é retornado um status de erro

--- test/functional/session.spec.js
+++ test/functional/session.spec.js
@@ -22,3 +22,21 @@ test('Retorna um token quando se autentica', async ({ assert, client }) => {
   response.assertStatus(200)
   assert.exists(response.body.token)
 })
+
+test('Retorna um erro se não encontrar o usuário', async ({ assert, client }) => {
+  const baseUser = {
+    username: 'teste4',
+    email: 'test4@email',
+    password: '123'
+  }
+
+  await User.create(baseUser)
+
+  baseUser.email = 'fail@mail'
+
+  const response = await client.post('/sessions')
+    .send(baseUser)
+    .end()
+
+  response.assertStatus(401)
+})
