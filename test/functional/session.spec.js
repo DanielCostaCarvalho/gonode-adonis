2e87f637443a98089cd32cbee5376c9a5b400249
Daniel Costa Carvalho <daniel.carvalho@ftc.edu.br>
2020-03-10 11:30:34 -0300

    Criando rota de autenticação

---
+++ test/functional/session.spec.js
@@ -0,0 +1,24 @@
+'use strict'
+
+const { test, trait} = use('Test/Suite')('Session')
+
+trait('Test/ApiClient')
+
+const User = use('App/Models/User')
+
+test('Retorna um token quando se autentica', async ({ assert, client }) => {
+  const baseUser = {
+    username: 'teste',
+    email: 'test@email',
+    password: '123'
+  }
+
+  const user = await User.create(baseUser)
+
+  const response = await client.post('/sessions')
+    .send(baseUser)
+    .end()
+
+  response.assertStatus(200)
+  assert.exists(response.body.token)
+})
