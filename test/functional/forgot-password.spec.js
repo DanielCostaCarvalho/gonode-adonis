e9400b430706356efcd0e55d308d18899a4bfeba
Daniel Costa Carvalho <daniel.carvalho@ftc.edu.br>
2020-03-13 11:11:21 -0300

    Acrescentando teste de email enviado para a rota de recuperação da senha

--- test/functional/forgot-password.spec.js
+++ test/functional/forgot-password.spec.js
@@ -3,6 +3,7 @@
 const { test, trait } = use('Test/Suite')('ForgotPassword')
 
 trait('Test/ApiClient')
+trait('DatabaseTransactions')
 
 const Mail = use('Mail')
 
@@ -25,6 +26,30 @@ test('Cadastrar token ao chamar rota de senha esquecida', async ({ assert, clien
 
   response.assertStatus(200)
   assert.isNotNull(user.token)
+
+  Mail.restore()
+})
+
+test('Enviar email ao chamar rota de senha esquecida', async ({ assert, client }) => {
+  const baseUser = {
+    username: 'teste2',
+    email: 'test2@email',
+    password: '123'
+  }
+
+  const user = await User.create(baseUser)
+
+  Mail.fake()
+
+  const response = await client.post('/forgotpassword')
+    .send(baseUser)
+    .end()
+
+  const recentEmail = Mail.pullRecent()
+
+  assert.include(recentEmail.envelope.to, baseUser.email)
+
+  Mail.restore()
 })
 
 test('Retornar erro ao chamar rota de senha esquecida com email inexistente', async ({ client }) => {
